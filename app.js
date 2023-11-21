69cd2b1e9d7c9f4c2b3be5a432aeb38a6306360c
Luca Lambiase <luca.lambia@gmail.com>
2023-11-21 12:03:31 +0100

    add correzione mail

--- app.js
+++ app.js
@@ -4,29 +4,46 @@ createApp({
     data() {
         return {
             user: "Utente Anonimo",
-            userNumber: ""
+            userNumber: "",
+            emails: [],
+            apiUrl: "https://flynn.boolean.careers/exercises/api/"
         }
     },
     methods: {
+        generaMail() {
+            for (let i = 0; i < 10; i++) {
+                
+                axios.get(this.apiUrl + ` /random/mail?nocache=${i}` ).then(resp => {
+                    this.emails.push( resp.data.response );
+                });
+                
+            }
+        },
         generaNumero() {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/int').then(resp => {
+            axios.get(this.apiUrl + '/random/int').then(resp => {
                 this.userNumber = resp.data.response;
+            });
+        },
+        generaNome() {
+            
+            // let self = this; //da usare con axios.get(...).then(function(resp){ ... })
+
+            axios.get(this.apiUrl + '/random/name').then(resp => {
+                // resp.data = dati ottenuti in risposta dal server
+                // resp.status = codice HTTP di risposta (es. 200)
+                // resp.statusText = codice HTTP di risposta (es. OK)
+                console.log("Ricevuto: ", resp.data);
+
+                //se la risposta è ok
+                if(resp.data.success) {
+                    this.user = resp.data.response;
+                }
             });
         }
     },
     mounted() {
+        this.generaNome();
+        this.generaMail();
 
-        axios.get('https://flynn.boolean.careers/exercises/api/random/name').then(resp => {
-            // resp.data = dati ottenuti in risposta dal server
-            // resp.status = codice HTTP di risposta (es. 200)
-            // resp.statusText = codice HTTP di risposta (es. OK)
-            console.log("Ricevuto: ", resp.data);
-
-            //se la risposta è ok
-            if(resp.data.success) {
-                this.user = resp.data.response;
-            }
-        });
-        
     }
 }).mount('#app')
